64341bc309fce06093fef10cbc16a7e3fc1c8159
jordanna12 <137913329+jordanna12@users.noreply.github.com>
2024-10-24 11:47:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,37 @@
+criaCartao(
+    'Volei',
+    'Quantas medalhas olimpicas a seleção feminina de volei tem ?',
+    '2 de ouro, 1 de prata e 3 de bronze totalizando 6'
+)
+
+criaCartao(
+    'Volei',
+    'Quantas posições tem no volêi?',
+    '5 posições'
+)
+
+criaCartao(
+    'Volei',
+    'Quais são essas posições?',
+    'Levantador, Oposto, Ponteiro (Pontas), Central ou meio-de-rede, Líbero'
+)
+
+criaCartao(
+    'Volei',
+    'Em ano o volei entrou nas olimpiadas?',
+    'em 1968, em Toquio'
+)
+
+
+criaCartao(
+    'Volei',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+
+criaCartao(
+    'Volei',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
